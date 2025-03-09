3681c8a991c121eaed38fe1804b16106f66daaa1
karan bodkhe <karanbodkhe757@gmail.com>
2025-03-10 01:00:21 +0530

    Initial commit

---
+++ server/routes/userRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { createUser,bookVisit,AllBookings, cancelBooking, getAllFavourites,toFav } from '../controllers/userController.js'
+const router = express.Router()
+
+router.post('/register',createUser)
+router.post('/bookVisit/:id', bookVisit)
+router.post('/allBookings',AllBookings)
+router.post('/removeBooking/:id',cancelBooking)
+router.post('/toFav/:rid',toFav)
+router.post('/allFav',getAllFavourites)
+
+export {router as userRoute}
